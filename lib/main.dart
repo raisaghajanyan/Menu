1f36f9c7a030a7955928405b62adb2237e03e9e4
Tigran Elyazyan <elyazyan.t@gmail.com>
2019-05-24 20:25:56 +0400

    Create first flutter project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+main(){
+ runApp(MyApp());
+
+}
+
+
+class MyApp extends StatelessWidget {
+  build(context ){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('first app'),
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+        body: Container(),
+
+      ),
+    );
+  }
+
+}
